29ef3735ca450773202189c70c67ab8432a33b20
이상헌 <pod3275@gmail.com>
2017-11-03 05:42:35 +0900

    creating and classifying data photo files

--- CAPTCHA.cpp
+++ CAPTCHA.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <opencv2/opencv.hpp>
+
 
 using namespace std;
 using namespace cv;
@@ -8,155 +10,210 @@ using namespace cv;
 int GetPixelData(Mat image, uchar* scale, int j, int i);
 
 int main(){
-	Mat image = imread("C:/Users/lawle/Documents/졸업논문/인터파크/0002.png", CV_LOAD_IMAGE_GRAYSCALE);
+	int count;
+	for (count = 500; count < 501; count++) {
 
-	if (image.empty()) {
-		std::cout << "파일을 불러올 수 없습니다." << endl;
-		return -1;
-	}
+		cout << count << endl;
 
-	namedWindow("원본", CV_WINDOW_AUTOSIZE);
-	imshow("원본", image);	
+		char NUM[7] = "0";
+		char buffer[30];
+		char *it;
+		it = _itoa(count, buffer, 10);
+		strcat(NUM, it);
 
-	uchar *scale;
-	int i, j, check = 0;
-	int sRow, sCol, bRow, bCol;
+		char loc[] = "C:/Users/lawle/Documents/졸업논문/인터파크/";
+		strcat(loc, NUM);
+		strcat(loc, ".png");
+		Mat image = imread(loc, CV_LOAD_IMAGE_GRAYSCALE);
 
-	int nRow = image.rows;
-	int nCol = image.cols;
+		if (image.empty()) {
+			std::cout << "파일을 불러올 수 없습니다." << endl;
+			return -1;
+		}
 
-	cout << image.rows << " " << image.cols << endl;
+		uchar *scale;
+		int i, j, check = 0;
+		int sRow, sCol, bRow, bCol;
 
-	for (i = 0; i < nCol; i++) {
-		for (j = 0; j < nRow; j++) {
-			scale = nullptr;
+		int nRow = image.rows;
+		int nCol = image.cols;
 
-			if (GetPixelData(image, scale, j, i) < 200) {
-				sRow = j;
-				sCol = i;
-				check = 1;
+		for (i = 0; i < nCol; i++) {
+			for (j = 0; j < nRow; j++) {
+				scale = nullptr;
+
+				if (GetPixelData(image, scale, j, i) < 200) {
+					sRow = j;
+					sCol = i;
+					check = 1;
+					break;
+				}
+			}
+			if (check == 1)
 				break;
-			}
 		}
-		if (check == 1)
-			break;
-	}
 
-	cout << sRow << " " << sCol << endl;
+		check = 0;
 
+		for (j = nRow - 2; j >= 0; j--) {
+			for (i = nCol - 2; i >= 0; i--) {
+				scale = nullptr;
 
-	check = 0;
-
-	for (j = nRow - 2; j >= 0; j--) {
-		for (i = nCol - 2; i >= 0; i--) {
-			scale = nullptr;
-
-			if (GetPixelData(image, scale, j, i) < 200) {
-				bRow = j;
-				bCol = i;
-				check = 1;
+				if (GetPixelData(image, scale, j, i) < 200) {
+					bRow = j;
+					bCol = i;
+					check = 1;
+					break;
+				}
+			}
+			if (check == 1)
 				break;
-			}
 		}
-		if (check == 1)
-			break;
-	}
-	cout << bRow << " " << bCol << endl;
 
-	Rect rect(sCol, sRow, bCol-sCol, bRow-sRow);
+		Rect rect(sCol, sRow, bCol - sCol, bRow - sRow);
 
-	Mat subimage = image(rect);
+		Mat subimage = image(rect);
 
-	nRow = subimage.rows;
-	nCol = subimage.cols;
+		nRow = subimage.rows;
+		nCol = subimage.cols;
 
-	for (i = 0; i < nCol; i++) {
-		for (j = 0; j < nRow; j++) {
-			scale = nullptr;
+		for (i = 0; i < nCol; i++) {
+			for (j = 0; j < nRow; j++) {
+				scale = nullptr;
 
-			if (GetPixelData(subimage, scale, j, i) < 100) {
-				subimage.at<uchar>(j, i) = 255;
-			}
-		}
-	}
-
-	for (i = 0; i < nCol; i++) {
-		for (j = 0; j < nRow; j++) {
-			scale = nullptr;
-
-			if (GetPixelData(subimage, scale, j, i) != 255) {
-				subimage.at<uchar>(j, i) = 0;
-			}
-		}
-	}
-
-	Mat medianimage;
-	Mat eroded;
-	Mat dilated;
-
-	dilate(subimage, dilated, Mat());
-	erode(dilated, eroded, Mat());
-	medianBlur(eroded, medianimage, 3);
-	namedWindow("d+e+m", CV_WINDOW_AUTOSIZE);
-	imshow("d+e+m", medianimage);
-
-	for (i = 0; i < nCol; i++) {
-		for (j = 0; j < nRow; j++) {
-			scale = nullptr;
-
-			if (i < 25 || j < 25 || i > (nCol-25) || j > (nRow-25)) {
-				medianimage.at<uchar>(j, i) = 255;
-			}
-		}
-	}
-
-
-	int start = 0, count = 0, st_col = 0, fin_col = 0, buf = 0;
-
-	Mat Letter[6];
-
-	for (i = 0; i < nCol; i++) {
-		count = 0;
-
-		for (j = 0; j < nRow; j++) {
-			scale = nullptr;
-			if (GetPixelData(medianimage, scale, j, i) == 0) {
-				count++;
-				if (start == 0) {
-					start++;
-					st_col = i;
+				if (GetPixelData(subimage, scale, j, i) < 100) {
+					subimage.at<uchar>(j, i) = 255;
 				}
 			}
 		}
 
-		if (start != 0 && count == 0) {
-			fin_col = i;
-			if (fin_col - st_col > 10) {
-				Rect rect2(st_col, 0, fin_col - st_col - 1, nRow - 1);
-				Letter[buf] = medianimage(rect2);
-				buf++;
+		for (i = 0; i < nCol; i++) {
+			for (j = 0; j < nRow; j++) {
+				scale = nullptr;
+
+				if (GetPixelData(subimage, scale, j, i) != 255) {
+					subimage.at<uchar>(j, i) = 0;
+				}
 			}
-			start = 0;
 		}
+
+		Mat medianimage;
+		Mat eroded;
+		Mat dilated;
+
+		dilate(subimage, dilated, Mat());
+		erode(dilated, eroded, Mat());
+		dilate(eroded, medianimage, Mat());
+
+		for (i = 0; i < nCol; i++) {
+			for (j = 0; j < nRow; j++) {
+				scale = nullptr;
+
+				if (i < 25 || j < 25 || i >(nCol - 25) || j >(nRow - 25)) {
+					medianimage.at<uchar>(j, i) = 255;
+				}
+			}
+		}
+
+		int start = 0, count = 0, st_col = 0, fin_col = 0, st_row = 0, fin_row = 0, buf = 0, nletter = 0;
+
+		Mat Letter[6];
+
+		for (i = 0; i < nCol; i++) {
+			count = 0;
+
+			for (j = 0; j < nRow; j++) {
+				scale = nullptr;
+				if (GetPixelData(medianimage, scale, j, i) == 0) {
+					count++;
+					if (start == 0) {
+						start++;
+						st_col = i;
+					}
+				}
+			}
+
+			if (start != 0 && count == 0) {
+				fin_col = i;
+				if (fin_col - st_col > 10) {
+					Rect rect2(st_col, 0, fin_col - st_col - 1, nRow - 1);
+					Letter[buf] = medianimage(rect2);
+					buf++;
+				}
+				start = 0;
+			}
+		}
+
+
+		start = 0; nletter = buf;
+
+		Mat Letter2[6];
+
+		for (buf = 0; buf < nletter; buf++) {
+			nRow = Letter[buf].rows;
+			nCol = Letter[buf].cols;
+
+			for (j = 0; j < nRow; j++) {
+				count = 0;
+
+				for (i = 0; i < nCol; i++) {
+					scale = nullptr;
+					if (GetPixelData(Letter[buf], scale, j, i) == 0) {
+						count++;
+						if (start == 0) {
+							start++;
+							st_row = j;
+						}
+					}
+				}
+
+				if (start != 0 && count == 0) {
+					fin_row = j;
+					if (fin_row - st_row > 10) {
+						Rect rect2(0, st_row, nCol - 1, fin_row - st_row - 1);
+						Letter2[buf] = Letter[buf](rect2);
+					}
+					start = 0;
+				}
+			}
+		}
+
+		for (i = 0; i < 6; i++) resize(Letter2[i], Letter2[i], Size(32, 32));
+		
+		char filename1[20] = "img";
+		strcat(filename1, NUM);
+		strcat(filename1, "_1.jpg");
+		imwrite(filename1, Letter2[0]);
+
+		char filename2[20] = "img";
+		strcat(filename2, NUM);
+		strcat(filename2, "_2.jpg");
+		imwrite(filename2, Letter2[1]);
+
+		char filename3[20] = "img";
+		strcat(filename3, NUM);
+		strcat(filename3, "_3.jpg");
+		imwrite(filename3, Letter2[2]);
+
+		char filename4[20] = "img";
+		strcat(filename4, NUM);
+		strcat(filename4, "_4.jpg");
+		imwrite(filename4, Letter2[3]);
+
+		char filename5[20] = "img";
+		strcat(filename5, NUM);
+		strcat(filename5, "_5.jpg");
+		imwrite(filename5, Letter2[4]);
+
+		char filename6[20] = "img";
+		strcat(filename6, NUM);
+		strcat(filename6, "_6.jpg");
+		imwrite(filename6, Letter2[5]);
+
+		waitKey(0);
+
 	}
 
-	namedWindow("Letter1", CV_WINDOW_AUTOSIZE);
-	imshow("Letter1", Letter[0]);
-	namedWindow("Letter2", CV_WINDOW_AUTOSIZE);
-	imshow("Letter2", Letter[1]);
-	namedWindow("Letter3", CV_WINDOW_AUTOSIZE);
-	imshow("Letter3", Letter[2]);
-	namedWindow("Letter4", CV_WINDOW_AUTOSIZE);
-	imshow("Letter4", Letter[3]);
-	namedWindow("Letter5", CV_WINDOW_AUTOSIZE);
-	imshow("Letter5", Letter[4]);
-	namedWindow("Letter6", CV_WINDOW_AUTOSIZE);
-	imshow("Letter6", Letter[5]);
-
-	namedWindow("제거", CV_WINDOW_AUTOSIZE);
-	imshow("제거", medianimage);
-
-	waitKey(0);
 	return 0;
 }
 
